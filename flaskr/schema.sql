9c2367bf9554a11fdce75665b1fbaebaef9c4e68
Syl <kirkkleinsasser@gmail.com>
2022-03-30 17:57:48 -0400

    added documentation and default settings

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,19 +1,28 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS throttle;
-DROP TABLE IF EXISTS post;
 
-CREATE TABLE user (
+CREATE TABLE user ( --create user table
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
 
-CREATE TABLE settings (
+CREATE TABLE settings ( --create settings table
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  throttle FLOAT NOT NULL,
+  throttle FLOAT NOT NULL UNIQUE,
   nightvision BOOLEAN NOT NULL,
   buttoncontrol BOOLEAN NOT NULL,
   keycontrol BOOLEAN NOT NULL,
-  vresolution INT NOT NULL,
-  hresolution INT NOT NULL
+  hresolution INT NOT NULL,
+  vresolution INT NOT NULL
 );
+
+INSERT INTO settings VALUES( --insert default values
+  1, --id
+  0.4, --throttle
+  1, --nightvision mode (on)
+  1, --button mode (on)
+  1, --key mode (on)
+  640, --hresolution
+  480 --vresolution
+)
