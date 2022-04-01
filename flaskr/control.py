5fbe70ee897830a2bd81da45a93039929db03d21
Syl <kirkkleinsasser@gmail.com>
2022-04-01 17:00:57 -0400

    close db command

--- flaskr/control.py
+++ flaskr/control.py
@@ -39,6 +39,7 @@ bp = Blueprint('control', __name__)
 def getsettings():
     db = get_db()
     settings = db.execute( 'SELECT throttle, nightvision, buttoncontrol, keycontrol, resolution FROM settings WHERE id = 1' )
+    session.clear()
     print(settings)
     return Response(settings)
 
