d2823815d90f82e085cb113f74835a2315d93147
Syl <kirkkleinsasser@gmail.com>
2022-04-24 00:53:31 -0400

    add an option to change username

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -32,7 +32,7 @@ def login_required(view):
     return wrapped_view
 
 @bp.route('/register', methods=('GET', 'POST'))
-@login_required #cannor create new user unless already registered!
+@login_required #cannot create new user unless already registered!
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -58,7 +58,8 @@ def register():
             else:
                 return redirect(url_for("auth.login"))
 
-        flash(error)
+        if error is not None:
+            flash(error) 
 
     return render_template('auth/register.html')
 
@@ -91,26 +92,43 @@ def login():
 @login_required
 def user():
     if request.method == 'POST':
-        oldpassword = request.form['oldpassword']
-        password = request.form['newpassword']
-        user_id = session["user_id"]
-
         db = get_db()
-
+        user_id = session["user_id"]
         error = None
+        if "change_password" in request.form:
+            oldpassword = request.form['oldpassword']
+            newpassword = request.form['newpassword']
 
-        if not oldpassword or not password: #check that the form is complete
-            error = 'All fields are required.'
+            if not oldpassword or not newpassword: #check that the form is complete
+                error = 'All fields are required.'
 
-        user_data = db.execute('SELECT * FROM user WHERE id = {}'.format(user_id,)).fetchone() #grab user data
+            user_data = db.execute('SELECT * FROM user WHERE id = {}'.format(user_id,)).fetchone() #grab user data to check old passowrd
 
-        if user_data is None: #catch database errors
-            error = 'Database error.'
-        elif not check_password_hash(user_data['password'], oldpassword): #check that the old password is correct
-            error = 'Incorrect password.'
+            if user_data is None: #catch database errors
+                error = 'Database error.'
+            elif not check_password_hash(user_data['password'], oldpassword): #check that the old password is correct
+                error = 'Incorrect password.'
 
-        print("UPDATE user SET password = '{}' WHERE id = '{}'".format(generate_password_hash(password),user_id))
-        db.execute("UPDATE user SET password = '{}' WHERE id = '{}'".format(generate_password_hash(password),user_id)) #update password
-        db.commit()
+            print("Change Password Query: UPDATE user SET password = '{}' WHERE id = '{}'".format(generate_password_hash(newpassword),user_id))
+            db.execute("UPDATE user SET password = '{}' WHERE id = '{}'".format(generate_password_hash(newpassword),user_id)) #update password
+            db.commit()
+        elif "change_username" in request.form:
+            password = request.form['password']
+            newusername = request.form['newusername']
+            
+            if not password or not newusername: #check that the form is complete
+                error = 'All fields are required.'
+
+            user_data = db.execute('SELECT * FROM user WHERE id = {}'.format(user_id,)).fetchone() #grab user data to check password
+
+            if user_data is None: #catch database errors
+                error = 'Database error.'
+            elif not check_password_hash(user_data['password'], password): #check that the password is correct
+                error = 'Incorrect password.'
+
+            print("Change Username Query: UPDATE user SET username = '{}' WHERE id = '{}'".format(newusername,user_id))
+            db.execute("UPDATE user SET username = '{}' WHERE id = '{}'".format(newusername,user_id)) #update username
+            db.commit()
+        flash(error)
 
     return render_template('auth/user.html')
