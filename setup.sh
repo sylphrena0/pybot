8c8ddd2e1f06a54ea0755ea594c8942d3d41f045
sylphrena0 <ksylphrena@gmail.com>
2023-04-16 20:15:14 -0400

    added init-db

--- setup.sh
+++ setup.sh
@@ -25,3 +25,6 @@ echo 'export FLASK_APP=pybot' >> ~/.bashrc
 echo 'export FLASK_ENV=development' >> ~/.bashrc 
 echo 'alias car="cd /home/pi/pybot; flask run -h 0.0.0.0"' >> ~/.bashrc 
 source ~/.bashrc 
+
+cd /home/pi/pybot
+flask init-db
